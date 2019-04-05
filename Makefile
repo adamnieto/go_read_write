86f29302efa29e1f398db79451dd6d47c8ce617e
adamnieto <anieto1@binghamton.edu>
2019-04-04 20:56:51 -0400

    Adds more to cmap

--- Makefile
+++ Makefile
@@ -3,6 +3,12 @@ all: emerging
 emerging: emerging.go cmap.go lmap.go
 	go build emerging.go cmap.go lmap.go
 
+test-chan: emerging
+	./emerging -chan -readers=2 -askers=2 -askdelay=10 -infiles=data/pg1041.txt,data/pg1103.txt
+
+test-lock: emerging
+	./emerging -lock -readers=2 -askers=2 -askdelay=10 -infiles=data/pg1041.txt,data/pg1103.txt
+
 .PHONY: clean
 
 clean:
--- cmap.go
+++ cmap.go
@@ -1,6 +1,7 @@
 package main
 
 type ChannelMap struct {
+  words map[string]int
 }
 
 func (c *ChannelMap) Listen() {
@@ -10,21 +11,30 @@ func (c *ChannelMap) Stop() {
 }
 
 func (c *ChannelMap) Reduce(functor ReduceFunc, accum_str string, accum_int int) (string, int) {
-	_ = functor
-	_ = accum_str
-	_ = accum_int
-	return "", 0
+	//_ = functor
+	//_ = accum_str
+	//_ = accum_int
+	//return "", 0
+  for k, v := range c.words {
+    accum_str, accum_int = functor(accum_str, accum_int, k, v)
+  }
+  return accum_str, accum_int
 }
 
 func (c *ChannelMap) AddWord(word string) {
-	_ = word
+	//_ = word
+  c.words[word]++
 }
 
 func (c *ChannelMap) GetCount(word string) int {
-	_ = word
-	return 0
+	//_ = word
+	//return 0
+  return c.words[word] 
 }
 
 func NewChannelMap() *ChannelMap {
-	return &ChannelMap{}
+	//return &ChannelMap{}
+  cm := new(ChannelMap)
+  cm.words = make(map[string]int)
+  return cm
 }
